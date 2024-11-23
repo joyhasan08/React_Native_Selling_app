6a0482ed7fe4a1f9d6b1b0b171ef44f651fcb1c8
nahid-diu-cse <nahid.diu.cse@gmail.com>
2024-11-23 18:17:51 +0600

    2nd commit

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView,Image } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView,Image,TouchableWithoutFeedback,TouchableOpacity, Button,Alert } from 'react-native';
 
 export default function App() {
   // console.log('hello');
@@ -7,13 +7,38 @@ export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       
-      <Text onPress={() => console.log('hello clicked')} style={styles.header} >Sell_Hobe  </Text>
+      <Text onPress={() => console.log('hello clicked')} style={styles.header} >Sell_Hobe </Text>
       <StatusBar style="auto" />
 
       <View>
-        <Image style={styles.img} source={
-          require('./assets/programming.png')} ></Image>
+        <TouchableOpacity style={styles.able} onPress={() => console.log('image clicked')}>
+
+          <Image resizeMode='contain' style={styles.img} source={
+          require('./app/assets/splash-icon.png')} >
+          </Image>
+        </TouchableOpacity>
+
+        <Text numberOfLines={4} lineBreakStrategyIOS='clip' style={{fontSize: '16', padding: '20'}}>
+          🚀 Ready for a deep dive? 
+          Check out my full course: https://mosh.link/react-native-course 
+          Subscribe for more awesome content: https://goo.gl/6PYaGF
+
+          ✋ Stay connected:
+          Twitter:   / moshhamedani  
+          Facebook:  / moshhamedani
+        </Text>
+
       </View>
+
+      <Button title='Click Me' onPress={() => alert('Button Clicked')} />
+
+      <Button title='Click Me Again' onPress={() => 
+        Alert.alert("My Title" ,"My Message",[{text: 'OK', text: 'Ignore me'}])} />
+
+      <Button title='Click Prompt' onPress={() => 
+        Alert.prompt('My Title 2', 'My massage 2', text => console.log(text))
+      } />
+
     </SafeAreaView>
   );
 }
@@ -31,18 +56,27 @@ const styles = StyleSheet.create({
     fontSize: '40',
     padding: '20',
     backgroundColor: 'teal',
-    borderEndColor: 'green',
-    borderRadius: '20',
-    borderTopEndRadius: '40'
+    borderColor: 'skyblue',
+    borderWidth: 5,
+    borderRadius: 10,
+    textAlign: 'center',
+    fontFamily: 'Courier New',
+    margin: '20',
+    borderCurved: true,
+    borderStyle: 'solid',
+
+    
   },
   img : {
-    width: '50%',
+    width: '100%',
+    height: '100%',
+    alignSelf: 'center',
+  },
+  able: {
+    width: '70%',
     height: '50%',
     alignSelf: 'center',
-    marginTop: '20',
-    borderRadius: '20',
-    blurRadius: '10',
-    
-    
+    marginTop: 20,
+    marginBottom: 20  
   }
 });
---
+++ app/screens/LoginScreen.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function LoginScreen(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default LoginScreen;
+
+const styles = StyleSheet.create({
+    
+})
